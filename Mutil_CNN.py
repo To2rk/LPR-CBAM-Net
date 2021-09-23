946f45a23e3df6704691568cf28e01f7f871a855
To2rk <ltybc1970@gmail.com>
2021-09-23 10:20:26 +0800

    update

--- Mutil_CNN.py
+++ Mutil_CNN.py
@@ -5,52 +5,40 @@ from torch import nn
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 
-
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, classes):
         super(Net, self).__init__()
-        # 第一层神经网络
-        # nn.Sequential: 将里面的模块依次加入到神经网络中
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1), # 3通道变成16通道，图片大小：44*140
+            nn.Conv2d(input_size[0], 16, kernel_size=5),# 图片大小：44*140
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2)                             # 图片大小：22*70
         )
-        # 第2层神经网络
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 64, kernel_size=3),           # 16通道变成64通道，图片大小：20*68
+            nn.Conv2d(16, 64, kernel_size=3),           # 图片大小：20*68
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)                             # 图片大小：10*34
         )
-        # 第3层神经网络
         self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3),          # 16通道变成64通道，图片大小：8*32
+            nn.Conv2d(64, 128, kernel_size=3),          # 图片大小：8*32
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2)                             # 图片大小：4*16，通道数 128
+            nn.MaxPool2d(2)                             # 图片大小：4*16
         )
-        # 第4层神经网络
         self.fc1 = nn.Sequential(
-            # in_features由输入张量的形状决定，out_features则决定了输出张量的形状 
-            # nn.Linear(in_features, out_features)
-            # out_feature：全连接层的神经元个数
             nn.Linear(4*16*128, 4096),
-            nn.Dropout(0.4),  
-            nn.ReLU()
-        )
-        # 第5层神经网络
-        self.fc2 = nn.Sequential(
-            nn.Linear(4096, 1024),
             nn.Dropout(0.2),  
             nn.ReLU()
         )
-        # 第6层神经网络
-        self.fc3 = nn.Linear(1024, 6*11)                 # 6:验证码的长度， 11: 字母列表的长度
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, 2048),
+            nn.Dropout(0.2),  
+            nn.ReLU()
+        )
+        self.fc3 = nn.Linear(2048, classes)
 
-    #前向传播
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
@@ -78,23 +66,32 @@ class CaptchaData(Dataset):
 
 # 车牌向量转为文本
 def vec2text(vec):
-    label = torch.nn.functional.softmax(vec, dim =1)
-    vec = torch.argmax(label, dim=1)
-    for v in vec:
-        text_list = [plate_list[v] for v in vec]
-    return ''.join(text_list)
+    label_list =[]
+    first_vec, others_vec = vec.split([len(labels), (len(labels)-1) * (plate_len -1)],dim=1)
 
-net = Net()
-plate_list = list('0123456789A')
-plate_length = 6
+    first_vec = torch.argmax(first_vec, dim=1)
+    label_list.append(labels[first_vec])
+
+    others_vec = others_vec.view(plate_len-1,-1)
+    others_vec = torch.argmax(others_vec, dim=1)
+    for item in others_vec:
+        label_list.append(labels[item])
+    label = "".join(label_list)
+    return label
+
+net = Net(input_size=[1, 48, 144], classes = 61)
+labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A']
+plate_len = 6
 
 
 # 图像处理
 def pre_img(img):
-    resize = cv2.resize(img, (140, 44))
-    gray = cv2.cvtColor(resize,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(1,1),0)  #(3,3)为高斯半径
-    _, binary = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    resized = cv2.resize(img, (144, 48))
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    binary = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+
+    # blur = cv2.GaussianBlur(gray,(1,1),0)  #(3,3)为高斯半径
+    # _, binary = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     return binary
 
@@ -104,13 +101,13 @@ def get_label(model_path, crop_img):
 
     if os.path.exists(model_path):
         checkpoint = torch.load(model_path)
-        net.load_state_dict(checkpoint['model_state_dict'])
+        net.load_state_dict(checkpoint)
 
     def predict(inputs):
         net.eval()
         with torch.no_grad():
             outputs = net(inputs)
-            outputs = outputs.view(-1, len(plate_list)) 
+            # outputs = outputs.view(-1, len(plate_list)) 
         return vec2text(outputs)
 
     transform = transforms.Compose([transforms.ToTensor()]) 
